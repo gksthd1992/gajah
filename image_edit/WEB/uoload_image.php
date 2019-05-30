69f0d0114112ecf6dd1f71c563f8efd88ed5094a
SUYEOLKO <48445082+Kosy5464@users.noreply.github.com>
2019-05-30 15:46:32 +0900

    Create uoload_image.php

---
+++ image_edit/WEB/uoload_image.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+if ( isset($_FILES["file"]["type"]) )//변수가 있는지 없는지
+{
+  $max_size = 5000 * 10240; // 500 KB
+  $destination_directory = "upload/";
+  $validextensions = array("jpeg", "jpg", "png");
+
+  $temporary = explode(".", $_FILES["file"]["name"]);//.을 기준으로 배열 저장
+  $file_extension = end($temporary);//제일끝에 배열을 file_extension에 저장 
+
+  // We need to check for image format and size again, because client-side code can be altered
+  if ( (($_FILES["file"]["type"] == "image/png") ||
+        ($_FILES["file"]["type"] == "image/jpg") ||
+        ($_FILES["file"]["type"] == "image/jpeg")
+       ) && in_array($file_extension, $validextensions))
+  {
+    if ( $_FILES["file"]["size"] < ($max_size) )
+    {
+      if ( $_FILES["file"]["error"] > 0 )
+      {
+        echo "<div clasbtn btn-lg btn-primarys=\"alert alert-danger\" role=\"alert\">Error: <strong>" . $_FILES["file"]["error"] . "</strong></div>";
+      }
+      else
+      {
+        if ( file_exists($destination_directory . $_FILES["file"]["name"]) )
+        {
+          echo "<div class=\"alert alert-danger\" role=\"alert\">Error: File <strong>" . $_FILES["file"]["name"] . "</strong> already exists.</div>";
+        }
+        else
+        {
+          $sourcePath = $_FILES["file"]["tmp_name"];
+          $targetPath = $destination_directory . $_FILES["file"]["name"];
+          move_uploaded_file($sourcePath, $targetPath);
+
+          echo "<div class=\"alert alert-success\" role=\"alert\">";
+          echo "<p>Image uploaded successful</p>";
+          echo "<p>File Name: <a href=\"". $targetPath . "\"><strong>" . $targetPath . "</strong></a></p>";
+          echo "<p>Type: <strong>" . $_FILES["file"]["type"] . "</strong></p>";
+          echo "<p>Size: <strong>" . round($_FILES["file"]["size"]/1024, 2) . " kB</strong></p>";
+          echo "<p>Temp file: <strong>" . $_FILES["file"]["tmp_name"] . "</strong></p>";
+          echo "</div>";
+        }
+      }
+    }
+    else
+    {
+      echo "<div class=\"alert alert-danger\" role=\"alert\">The size of image you are attempting to upload is " . round($_FILES["file"]["size"]/1024, 2) . " KB, maximum size allowed is " . round($max_size/1024, 2) . " KB</div>";
+    }
+  }
+  else
+  {
+    echo "<div class=\"alert alert-danger\" role=\"alert\">Unvalid image format. Allowed formats: JPG, JPEG, PNG.</div>";
+  }
+}
+
+?>
+
